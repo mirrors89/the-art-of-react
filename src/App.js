58303b13a3370752d57da1d7407e02c588f560c7
mirrors89 <lee01494@gmail.com>
2018-10-30 16:46:26 +0900

    9장 컴포넌트 스타일링 - styled-component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-import Button from './component/Button'
+// import Button from './component/Button'
+import StyledButton from './component/StyledButton'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Button>버튼</Button>
+        <StyledButton big>버튼</StyledButton>
       </div>
     )
   }
