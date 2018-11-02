1939dc859870c81f0f2c05d934948afb447e666e
mirrors89 <lee01494@gmail.com>
2018-11-02 11:57:42 +0900

    10장 일정관리 웹 애플리케이션 생성 - 상태 관리

--- src/App.js
+++ src/App.js
@@ -4,12 +4,80 @@ import TodoInput from './components/TodoInput'
 import TodoList from './components/TodoList'
 
 class App extends Component {
+  state = {
+    input : '',
+    todos: [
+      {id: 0, text: '리액트 공부하기', done: true},
+      {id: 1, text: '컴포넌트 스타일링 해보기', done: false}
+    ]
+  }
+
+  id = 1
+  getId = () => {
+    return ++this.id
+  }
+
+  handleToggle = (id) => {
+    const { todos } = this.state
+    const index = todos.findIndex(todo => todo.id === id)
+
+    const toggled = {
+      ...todos[index],
+      done: !todos[index.done]
+    }
+
+    this.setState({
+      todos: [
+        ...todos.slice(0, index),
+        toggled,
+        ...todos.slice(index + 1, todos.length)
+      ]
+    })
+  }
+
+  handleChange = (e) => {
+    const { value } = e.target
+    this.setState({
+      input : value
+    })
+  }
+
+  handleInsert = () => {
+    const { todos, input } = this.state;
+
+    const newTodo = {
+      text: input,
+      done: false,
+      id: this.getId()
+    }
+
+    this.setState({
+      todos: [...todos, newTodo],
+      input: ''
+    })
+  }
+
+  handleRemove = (id) => {
+    const { todos } = this.state
+    const index = todos.findIndex(todo => todo.id === id)
+
+    this.setState({
+      todos: [
+        ...todos.slice(0, index),
+        ...todos.slice(index + 1, todos.length)
+      ]
+    })
+  }
+
   render() {
+    const { input, todos } = this.state
+    const { handleChange, handleInsert, handleToggle, handleRemove } = this;
+
     return (
       <div>
         <PageTemplate>
-          <TodoInput/>
-          <TodoList/>
+          <TodoInput onChange={handleChange} onInsert={handleInsert} value={input}/>
+          <TodoList todos={todos} onToggle={handleToggle} onRemove={handleRemove}/>
         </PageTemplate>
       </div>
     )
